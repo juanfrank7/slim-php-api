f18191201ecc5d3a23be136f6aa07227f5601c2b
Juan F Gonzalez <juano_gonza@hotmail.com>
2018-09-10 22:23:52 -0500

    changed connection file

--- src/database.php
+++ src/database.php
@@ -1,12 +1,14 @@
 <?php 
-    class db{
-        private $dbHost = 'localhost';
-        private $dbUser = 'admin';
-        private $dbPass = 'Fr4nc15c0';
-        private $dbName = 'deportes';
 
-        public function connectDB(){
-            $conn = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName) or die("Some error occurred during connection " . mysqli_error($con));
-            return $conn;
-        }
+function con(){    
+    $dbHost = 'localhost';
+    $dbUser = 'admin';
+    $dbPass = 'Fr4nc15c0';
+    $dbName = 'deportes';
+    static $conn = null;
+
+    if($conn === null){
+        $conn = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName);
     }
+    return $conn;
+}
